62e1f820b31c86f2125b3062cfb59c3b2b118175
Andres Paucar <35370608+andrespaucar@users.noreply.github.com>
2021-04-21 10:26:10 -0500

    Create feathericon.vue

---
+++ components/feathericon.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" 
+        stroke="currentColor" stroke-width="2" stroke-linecap="round" class="feather"
+        v-html="svgToHtml.replace(/<svg([^>]+)>/,'<svg >')">
+    </svg>
+</template>
+
+<script>
+import { onBeforeMount, onMounted, ref } from 'vue'
+export default {
+    props:{
+        name:{required:true},
+    },
+    setup(props){
+        const svgToHtml = ref('')
+        onMounted(()=>{
+            svgToHtml.value = feather.icons[props.name].toSvg()
+        })
+        onBeforeMount(()=>{})
+        return{
+            svgToHtml
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
